b3861df51b0a82380fcf8058e70aeba7b72c739e
Geoffrey Gibson <geoffrey.gibson50@gmail.com>
2025-01-29 12:23:12 -0600

    Delete expense-tracker directory

--- expense-tracker/src/main/java/com/expensetracker/repository/ExpenseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.expensetracker.repository;
-
-import com.expensetracker.entities.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {}
